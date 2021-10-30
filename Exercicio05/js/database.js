c429666df43248fa094abc2dafb2f78183a5fd89
Rafael Guedes <rafaelguedes.ufam@gmail.com>
2021-10-30 13:51:26 -0400

    Update Exercicio 5 - completo

--- Exercicio05/js/database.js
+++ Exercicio05/js/database.js
@@ -29,7 +29,7 @@ function verifyBrowser() {
 }
 
 // Função que cria a tabela Aluno
-function createDatabase() {
+function createTable() {
     db.transaction(function (tx) {
         createStudent = "CREATE TABLE IF NOT EXISTS students (" +
             "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
@@ -42,15 +42,75 @@ function createDatabase() {
     });
 }
 
+function insertStudent() {
+    //id = 284728;
+    nome = document.getElementById('nome').value;
+    esporte = document.getElementById('esporte').value;
+    telefone = document.getElementById('telefone').value;
+
+    db.transaction(function (tx) {
+        sql = `INSERT INTO students (nome,sport,telefone) VALUES (?,?,?);`;
+        console.log('SQL: ' + sql);
+        tx.executeSql(sql, [nome,esporte,telefone]);
+    });
+
+}
+
 function selectStudent() {
     db.transaction(function (tx) {
-        sql = 'SELECT * FROM students';
+        sql = 'SELECT * FROM students;';
         tx.executeSql(sql, [], function (tx, results) {
             var len = results.rows.length;
             alert('Existem ' + len + ' registros!');
             for (var i = 0; i < len; i++) {
                 var row = results.rows.item(i);
-                alert(`${row.id}: ${row.nome}, ${row.esporte}, ${row.telefone}`);
+                alert(`${row.id}: ${row.nome}, ${row.sport}, ${row.telefone}`);
+            }
+        },
+            function (tx, error) {
+                alert('Ooops ' + error.message);
+            });
+    });
+}
+
+function listTable() {
+
+    db.transaction(function (tx) {
+        let tbody = document.getElementById('tableBody');
+        tbody.innerText = '';
+
+        sql = 'SELECT * FROM students;';
+        tx.executeSql(sql, [], function (tx, results) {
+            var len = results.rows.length;
+            //console.log("Recuperando dados!");
+            for (var i = 0; i < len; i++) {
+                var row = results.rows.item(i);
+                let tr = tbody.insertRow();
+
+                let td_id = tr.insertCell();
+                let td_nome = tr.insertCell();
+                let td_esporte = tr.insertCell();
+                let td_telefone = tr.insertCell();
+                let td_acao = tr.insertCell();
+
+                console.log(`${row.id}: ${row.nome}, , ${row.sport}, ${row.telefone}`);
+                td_id.innerText = row.id;
+                td_nome.innerText = row.nome;
+                td_esporte.innerText = row.sport;
+                td_telefone.innerText = row.telefone;
+
+                let imgDelete = document.createElement('img');
+                imgDelete.src = 'image/trash_icon.svg';
+                imgDelete.setAttribute("class", "icon");
+                
+                //Botão deletar com imagem
+                let btnDel = document.createElement('button');
+                btnDel.setAttribute("onclick", "deleteStudent("+row.id+")");
+                btnDel.setAttribute("class", "button");
+                btnDel.appendChild(imgDelete);
+
+                td_acao.appendChild(btnDel);
+                //td_acao.appendChild(imgDelete);
             }
         },
             function (tx, error) {
@@ -61,23 +121,28 @@ function selectStudent() {
 
 // A ideia é receber o id do aluno pelo id na row em que o botão foi pressionado
 function deleteStudent(idStu) {
-    db.transaction(function (tx) {
-        sql = `DELETE FROM students WHERE id=${idStu}`; //'DELETE * FROM contacts WHERE id='+idStu
-        tx.executeSql(sql) //será que funciona?
-    })
-}
-
-function insertStudent() {
-    nome = document.getElementById('nome').value;
-    esporte = document.getElementById('esporte').value;
-    telefone = document.getElementById('telefone').value;
 
     db.transaction(function (tx) {
-        sql = `INSERT INTO students VALUES (${nome},${esporte},${telefone})`;
-        console.log('SQL: ' + sql);
-        tx.executeSql(sql, [], function(transaction, results){
-            successCallback(results);
-        }, errCallback);
-    })
-
+        sql = `DELETE FROM students WHERE id=${idStu};`; //'DELETE * FROM contacts WHERE id='+idStu
+        tx.executeSql(sql)
+        alert(`Valor com id=${idStu} apagado!`);
+        listTable();
+        // Atualizar lista na tabela
+    });
 }
+
+function deleteTable() {
+    //console.log("Deletando tabela");
+    db.transaction(function (tx) {
+        sql = 'DROP TABLE IF EXISTS students;';
+        tx.executeSql(sql);
+        alert("Tabela students deletada");
+        console.log("Function delete executada");
+    });
+}
+
+
+
+/*, function(transaction, results){
+    successCallback(results);
+}, errCallback*/
