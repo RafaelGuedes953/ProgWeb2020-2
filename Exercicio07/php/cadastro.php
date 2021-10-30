2257c9bf3a9ec3acc138d0cd18881d71f5c89a9a
Rafael Guedes <rafaelguedes.ufam@gmail.com>
2021-10-30 16:39:21 -0400

    Update Exercicio 07 - Cadastro de usuario

---
+++ Exercicio07/php/cadastro.php
@@ -0,0 +1,63 @@
+<?PHP
+
+
+# dados para conexão com o banco de dados
+
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'sistema_web';
+
+
+
+# executa a conexão com o MySQL
+$link = mysqli_connect("$servidor", "$usuario", "$senha", "$banco");
+//mysqli_select_db($link, 'test');
+
+$acao = $_SERVER['QUERY_STRING'];
+
+#verifica se o arquivo foi chamado a partir de um formulario
+if ($acao == "adicionar") {
+    //Recuperar valores enviados por POST
+    $Cpf = $_POST['cpf'];
+    $Senha = $_POST['pass'];
+    $Nome = $_POST['nome'];
+    $Sobrenome = $_POST['sobrenome'];
+
+    // Definição da expressão SQL (INSERT)
+
+    $sql = "INSERT INTO users (cpf, nome, sobrenome, pass) VALUES ('$Cpf', '$Nome', '$Sobrenome', '$Senha')";
+
+    //Executa a espressão no servidor
+    $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+
+    // Verifica erro na operação e guarda o resultado
+    if (!$result) {
+        die('Erro: ' . mysqli_error($link)); //verificar posteriormente
+    }
+    else {
+        // Caso sucesso, mostra na tela
+        echo 'A operação foi realizada com sucesso.';
+    }
+}
+?>
+
+<!--
+<center><a href="visualizar-registro.php">VISUALIZAR REGISTROS</a>
+    <p>
+</center>
+
+<br /><br />
+
+<center><a href="cadastrar-registro.php">INSERIR NOVO REGISTRO</a>
+    <p>
+</center>
+
+<br /><br />
+
+<center><a href="visualizarealterar-registro.php">ALTERAR REGISTRO</a></center>
+
+<br /><br />
+
+<center><a href="visualizareexcluir-registro.php">EXCLUIR REGISTRO</a></center>
+-->
